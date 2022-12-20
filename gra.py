b7bd7a63e340b06f383cfaf936301debc92e2d02
Piotr Bauer <65217796+Bauero@users.noreply.github.com>
2022-12-20 18:17:08 +0100

    gotowa gra

--- gra.py
+++ gra.py
@@ -30,6 +30,7 @@ def load_image(img_path, position):
 
 player_pos = [SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2]
 player = load_image("player.png",player_pos)
+background_color = [0,0,0]
 
 
 def print_image(img_lista):
@@ -39,6 +40,39 @@ def print_image(img_lista):
     screen_surface.blit(surface, rect)
 
 
+def set_position_image(img_list, position):
+
+    image, surface, rect = img_list
+
+    rect = surface.get_rect( center = position )
+
+    return [image, surface, rect]
+
+#przeliczanie pozycji gracza
+def calculate_player_movement(keys):
+
+    speed = 10
+    zmiana_x = 0
+    zmiana_y = 0
+ 
+    if keys[pygame.K_w]:
+        zmiana_y -= speed
+    if keys[pygame.K_s]:
+        zmiana_y += speed
+    if keys[pygame.K_d]:
+        zmiana_x += speed
+    if keys[pygame.K_a]:
+        zmiana_x -= speed
+
+    return [zmiana_x, zmiana_y]
+
+def limit_position(position):
+    x , y = position
+
+    x = max(0, min(x,SCREEN_WIDTH))
+    y = max(0, min(y,SCREEN_HEIGHT))
+
+    return [x,y]
 
 
 # tworzymy zmienną, która przechowuje stan gry (czyli czy gra działa czy nie działa)
@@ -60,6 +94,20 @@ while game_status:
         if event.type == pygame.QUIT:
             game_status = False
 
+        pressed_keys = pygame.key.get_pressed()
+
+        przes_x, przes_y = calculate_player_movement(pressed_keys)
+
+        player_pos[0] += przes_x
+        player_pos[1] += przes_y
+
+        player_pos = limit_position(player_pos)
+
+        player = set_position_image(player, player_pos)
+
+
+    screen_surface.fill(background_color)
+    
     #ładowanie naszego gracza na ekran
     print_image(player)
 
